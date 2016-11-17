54fdefc0273329d013990994d5b902b4b4637669
Jonathan A <iotku@iotku.pw>
2016-11-17 00:19:37 -0800

    Add initial script to start dazzle

---
+++ dazzle.sh
@@ -0,0 +1,3 @@
+v4lctl setinput s-video
+v4lctl setnorm ntsc
+ffmpeg -f v4l2 -video_size 640x480 -framerate 29.97 -thread_queue_size 50k -i /dev/video0 -thread_queue_size 50k -f jack -i ffmpeg -ac 2 -c:a aac -b:a 160k -ar 44100 -vcodec libx264 -preset veryfast -pix_fmt yuv420p  -profile:v high -level 4.0 -g 59.94  -b:v 1800k -minrate 1800k -maxrate 1800k -bufsize 1800k test.mp4
--- easy-configure.sh
+++ easy-configure.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "Select Window to capture."
 wininfo=$(xwininfo)
 width=$(echo "$wininfo" | grep Width  | cut -c 10-)
 height=$(echo "$wininfo" | grep Height | cut -c 11-)
@@ -7,3 +8,5 @@ posy=$(echo "$wininfo" | grep "Absolute upper-left Y" | cut -c 27-)
 
 echo SCREENCAP_AREA=\"${posx},${posy}\" > screencast-settings
 echo SCREENCAP_SIZE=\"${width}x${height}\" >> screencast-settings
+
+echo "Configured for area: x=${posx}, y=${posy}. Size: ${width}x${height}"
