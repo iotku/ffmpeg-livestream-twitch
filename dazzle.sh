0a4a80febeab4af438376c6f5a7c752176ba8e1f
Jonathan A <iotku@iotku.pw>
2017-05-04 17:02:38 -0700

    Don't use tee/localrecord yet, gives blackscreen on twitch...

--- dazzle.sh
+++ dazzle.sh
@@ -8,6 +8,6 @@ ffmpeg -f v4l2 -video_size 640x480 -framerate 29.97 -thread_queue_size 50k -i /d
     -f jack -i ffmpeg -ac 2 -c:a aac -b:a 160k -ar 44100 -vcodec libx264 -preset veryfast -pix_fmt yuv420p  -profile:v high -level 4.0 -g 59.94 \
     -b:v 1500k -minrate 1500k -maxrate 1500k -bufsize 1500k -vsync 1 \
     -filter_complex "[0:v]bwdif[deint];[deint]crop=w=570:h=448:x=28[deint];[1:v]scale=220:320[splits];[splits]pad=786:448[splits];[splits][deint]overlay=220:0[vid];[2:a]adelay=335|335[daudio]"\
-    -map [vid] -map [daudio]\
-    -f tee "localrecord.mkv|[f=flv]rtmp://${TWITCH_STREAM_URL_IOTKU}"
+    -map [vid] -map [daudio] \
+    -f flv "rtmp://${TWITCH_STREAM_URL_IOTKU}"
 
